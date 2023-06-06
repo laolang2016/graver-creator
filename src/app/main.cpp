94444eef91f5430bcca9cc08fc56e3835e492dee
laolang2016 <xiaodaima2016@163.com>
2023-06-06 21:52:45 +0800

    ✨ feat: 添加 spdlog

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,9 +1,16 @@
-#include "mainwindow.h"
-
 #include <QApplication>
+
+#include "mainwindow.h"
+#include "logutil.h"
+
 
 int main(int argc, char *argv[])
 {
+    LogUtil::init();
+
+    std::shared_ptr<spdlog::logger> log = LogUtil::getLogger("app");
+    log->info("hello, graver creator!");
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
